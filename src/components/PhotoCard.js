7d7b66863127aa9f3cf63cf8ee9b2794c6720b34
Adrian Kelly <hello@adriankelly.net>
2017-04-26 16:52:25 +0200

    Remove escape characters from photocard component author link url

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -6,7 +6,7 @@ const PhotoCard = (props) => {
   return (
     <div className="photocard">
         <img src={props.photo.media.m} alt=""/>
-        <span><a href={props.photo.link}>{props.photo.title}</a> by <a href={"https:\/\/www.flickr.com\/photos\/" + props.photo.author_id}><small>{props.photo.author.match(/"([^"]*)"/)[1]}</small></a></span>
+        <span><a href={props.photo.link}>{props.photo.title}</a> by <a href={"https://www.flickr.com/photos/" + props.photo.author_id}><small>{props.photo.author.match(/"([^"]*)"/)[1]}</small></a></span>
         <p>Description: {props.photo.description.replace(/<[^>]*>/g, '')}</p>
         {props.photo.tags.length > 0 &&
         <span>Tags: {props.photo.tags}</span>
