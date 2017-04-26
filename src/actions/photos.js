45de9c409769ae98bca0a481dd7ac1a174a57a58
Adrian Kelly <hello@adriankelly.net>
2017-04-26 12:52:58 +0200

    Create redux actions for photo fetching events

---
+++ src/actions/photos.js
@@ -0,0 +1,44 @@
+/**
+ * Action Creators
+ */
+
+// hasErrored and isLoading receive boolean values
+export function photosHasErrored(hasErrored) {
+  return { type: 'PHOTOS_HAS_ERRORED', hasErrored }
+}
+
+export function photosIsLoading(isLoading) {
+  return { type: 'PHOTOS_IS_LOADING', isLoading }
+}
+
+// API data passed in as json
+export function receivePhotos(photos, json) {
+  return {
+    type: 'RECEIVE_PHOTOS',
+    photos,
+    data: json.data.children.map(child => child.data),
+    receivedAt: Date.now()
+    }
+}
+
+
+/**
+ * Redux Thunk Action Creator
+ * Returns function allowing for asynchronous API calls
+ * Also dispatching synchronous Action Creators defined above
+ */
+export function fetchPhotos() {
+  return (dispatch) => {
+
+    dispatch(photosIsLoading(true));
+
+    return fetch('https://api.github.com') //test with github api
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        dispatch(photosIsLoading(false))
+        dispatch(receivePhotos(photos, json))
+      })
+      .catch(() => dispatch(photosHasErrored(hasErrored)));
+  }
+}
